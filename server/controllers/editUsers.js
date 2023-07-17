21203c37f06651e3a6ae5256c965e6bdc04076a8
anilpahari <anilpahari555@gmail.com>
2023-07-17 12:07:13 +0545

    edit

---
+++ server/controllers/editUsers.js
@@ -0,0 +1,22 @@
+const getConnection = require("./databaseConnection.js");
+const editUser = async (req, res) => {
+  try {
+    getConnection.query(
+      "SELECT * FROM users WHERE id=? ",
+      [req.params.id],
+
+      (err, rows) => {
+        if (err) {
+          console.log("Error retrieving data from the table:", err);
+        } else {
+          res.render("editUser", { rows });
+          console.log("The data from the table:\n", rows);
+          console.log(req.params.id);
+        }
+      }
+    );
+  } catch (error) {
+    console.log("An error occurred during the query execution:", error);
+  }
+};
+module.exports = editUser;
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,11 +3,13 @@ const view = require("../controllers/viewControllers");
 const searchItem = require("../controllers/searchControllers");
 const newUser = require("../controllers/newUser");
 const submitUser = require("../controllers/postUser");
+const editUser = require("../controllers/editUsers");
 const routes = express.Router();
 
 routes.get("/", view);
 routes.post("/", searchItem);
 routes.get("/newUser", newUser);
 routes.post("/newUser", submitUser);
+routes.get("/editUser/:id", editUser);
 
 module.exports = routes;
